8aa2dba161d50475137c8c3cc9349a136c6c7a3e
kallolpratim <ksaikia615@gmail.com>
2019-02-20 16:21:12 +0530

    Updates

---
+++ precache-manifest.9bb170b22faa82b6b3d3cc6b3572cdb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18c63570f970f4a41ea9",
+    "url": "/jedi-typist/static/js/runtime~main.18c63570.js"
+  },
+  {
+    "revision": "710f86fd2baac8de7095",
+    "url": "/jedi-typist/static/js/main.710f86fd.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/jedi-typist/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "710f86fd2baac8de7095",
+    "url": "/jedi-typist/static/css/main.071cd894.chunk.css"
+  },
+  {
+    "revision": "1c652e14eb12db8ebcc251728b122e64",
+    "url": "/jedi-typist/index.html"
+  }
+];
